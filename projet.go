979929ede57115f1a4b025db3431911e5cf3825b
mega <aurelien.lestrade@ynov.com>
2020-09-10 21:22:21 +0000

    initial commit

---
+++ projet.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello gays")
+	var echap = 650.0
+	var salaire = 45.0
+	var argent = 67.0
+
+	var temps = (echap - argent) / salaire
+	fmt.Println("il lui faudra", temps, "semaines pour acheter l'échappement")
+}
